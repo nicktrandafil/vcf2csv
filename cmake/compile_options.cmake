ec89de8e1d7ef0e161842636a53b0b0917d045b3
Николай Трандафил <nicktrandafil@gmail.com>
2017-09-09 22:43:19 +0300

    Initial commit

---
+++ cmake/compile_options.cmake
@@ -0,0 +1,23 @@
+# Compile options
+set(compile_options)
+if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+   set(compile_options
+    $<$<CONFIG:Release>:
+      -O2
+      -DNDEBUG
+    >
+    $<$<CONFIG:Debug>:
+      -O0
+      -ggdb3
+      -Wall
+    >
+  )
+endif()
+
+# Properties
+set(properties CXX_STANDARD 17)
+
+# Debug config shortcut
+if(CMAKE_BUILD_TYPE MATCHES "Debug")
+  set(debug)
+endif()
